cbd6d03e21140a6480e9068207e418974d88bc0d
Mestery <mestery@pm.me>
2021-05-29 23:30:05 +0200

    feat: create bins from explorer & refactor
    A bin can now be created from the file explorer,
    with a right click, then 'Create a bin',
    this will open the file in vscode.
    
    Also, the extension has been refactored.
    The 'Copy' button has been added.
    
    Closes https://github.com/readthedocs-fr/bin-client-vscode/issues/3.
    Closes https://github.com/readthedocs-fr/bin-client-vscode/issues/2.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,15 +1,7 @@
 # Changelog
-All notable changes to this project will be documented in this file.
 
-The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
-and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+# [v0.1.0](https://github.com/readthedocs-fr/bin-client-discord/releases/tag/v0.1.0) (2021-01-17)
 
-## [Unreleased]
+### Features
 
-# [v0.1.0] - 2021-01-17
-### Added
-
-- Easy creation of bins, in the context menu and the command palette
-
-[unreleased]: https://github.com/readthedocs-fr/bin-vscode/compare/v0.1.0...HEAD
-[v0.1.0]: https://github.com/readthedocs-fr/bin-vscode/releases/tag/v0.1.0
+- easy creation of bins, in the context menu and the command palette
--- src/extension.ts
+++ src/extension.ts
@@ -1,66 +1,90 @@
-import * as vscode from 'vscode';
+import { basename } from 'path';
+import { window, commands, workspace, env, Uri, ExtensionContext } from 'vscode';
 import { createBin } from './helpers/bin';
 import { input } from './helpers/input';
 
-export function activate(): void {
-  vscode.commands.registerCommand('rtdbin.createBin', async () => {
-    const window = vscode.window;
-    const editor = window.activeTextEditor;
+export function activate(context: ExtensionContext): void {
+	// eslint-disable-next-line sonarjs/cognitive-complexity
+	const disposable = commands.registerCommand('rtdbin.createBin', async (_, filePath?: Uri[]) => {
+		const editor =
+			// eslint-disable-next-line @typescript-eslint/no-empty-function
+			filePath ? await window.showTextDocument(filePath[0]).then(undefined, () => {}) : window.activeTextEditor;
 
-    if (!editor) {
-      window.showErrorMessage('There is no active text editor.');
-      return;
-    }
+		if (!editor) {
+			window.showErrorMessage('There is no active text editor.');
+			return;
+		}
 
-    const selection = editor.selection;
-    const code = editor.document.getText(selection.isEmpty ? undefined : selection);
+		const selection = editor.selection;
+		const code = editor.document.getText(selection.isEmpty ? undefined : selection);
 
-    const language = await input({
-      prompt: 'The language of your bin',
-      placeHolder: 'Language',
-      value: editor.document.languageId,
-    });
-    if (!language) {
-      return;
-    }
+		if (!code) {
+			return;
+		}
 
-    const configuration = vscode.workspace.getConfiguration('rtdbin');
+		const language = await input({
+			prompt: 'The language of your bin',
+			placeHolder: 'Language',
+			value: editor.document.languageId,
+		});
 
-    const lifetime = await input({
-      prompt: 'The lifetime of your bin (0 for never, else in this format: 1d1h1m1s)',
-      placeHolder: 'Expiration',
-      value: configuration.get('defaultExpiration'),
-      validateInput(value: string) {
-        return !/^[0-9dhms]+$/.test(value) ? 'Please enter a valid number (0 or 1d1h1m1s)' : undefined;
-      },
-    });
-    if (!lifetime) {
-      return;
-    }
+		if (!language) {
+			return;
+		}
 
-    const maxUsages = await input({
-      prompt: 'The max usages of your bin (0 for infinity)',
-      placeHolder: 'Max usages',
-      value: configuration.get('defaultMaxUsages'),
-      validateInput(value: string) {
-        const maxUsages = parseInt(value, 10);
-        return !isNaN(maxUsages) && maxUsages < 0
-          ? 'Please enter a valid number greater than or equal to 0'
-          : undefined;
-      },
-    });
-    if (!maxUsages) {
-      return;
-    }
+		const configuration = workspace.getConfiguration('rtdbin');
 
-    try {
-      const bin = await createBin({ code, lang: language, maxusages: maxUsages, lifetime });
-      const selection = await window.showInformationMessage(`[${bin}](${bin})`, 'Open');
-      if (selection === 'Open') {
-        vscode.env.openExternal(vscode.Uri.parse(bin));
-      }
-    } catch (error) {
-      window.showErrorMessage(error.toString());
-    }
-  });
+		const lifetime = await input({
+			prompt: 'The lifetime of your bin (0 for never, else in this format: 1d1h1m1s)',
+			placeHolder: 'Lifetime',
+			value: configuration.get('defaultLifetime'),
+			validateInput(value) {
+				return !/^[\ddhms]+$/.test(value) ? 'Please enter a valid duration (0 or 1d1h1m1s).' : undefined;
+			},
+		});
+
+		if (!lifetime) {
+			return;
+		}
+
+		const maxUses = await input({
+			prompt: 'The max uses of your bin (0 for infinity)',
+			placeHolder: 'Max uses',
+			value: configuration.get('defaultMaxUses'),
+			validateInput(value) {
+				// eslint-disable-next-line @typescript-eslint/no-shadow
+				const maxUses = parseInt(value, 10);
+				return isNaN(maxUses) || maxUses < 0 ? 'Please enter a valid integer greater than or equal to 0.' : undefined;
+			},
+		});
+
+		if (!maxUses) {
+			return;
+		}
+
+		try {
+			const binUrl = await createBin({
+				code,
+				filename: basename(editor.document.fileName),
+				extension: language,
+				maxUses: parseInt(maxUses),
+				lifetime: parseInt(lifetime),
+			});
+
+			const action = await window.showInformationMessage(`[${binUrl}](${binUrl})`, 'Open', 'Copy');
+
+			if (action === 'Open') {
+				env.openExternal(Uri.parse(binUrl));
+			} else {
+				env.clipboard.writeText(binUrl);
+			}
+		} catch (error) {
+			window.showErrorMessage(error);
+		}
+	});
+
+	context.subscriptions.push(disposable);
 }
+
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+export function deactivate(): void {}
